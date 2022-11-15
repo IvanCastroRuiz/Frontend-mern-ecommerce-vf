31f4bf279200edb3de973f4c2c1ba7e469b7f9a3
ijcastrosena <111159778+ijcastrosena@users.noreply.github.com>
2022-11-14 21:28:17 -0500

    first commit

---
+++ src/Layout/LayoutAuth.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+const LayoutAuth = () => {
+  return (
+    <div className="h-screen">
+      <div className="flex h-screen">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default LayoutAuth
